cc3b60a42dff30aa65938e26439857ac8eb1541f
harshitbansal0 <127868064+harshitbansal0@users.noreply.github.com>
2024-11-23 11:14:11 +0530

    test

---
+++ 2210990385_test2/RecipeCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeCellTableViewCell.swift
+//  2210990385_test2
+//
+//  Created by student-2 on 23/11/24.
+//
+
+import UIKit
+
+class RecipeCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 2210990385_test2/Recipes.swift
@@ -0,0 +1,8 @@
+//
+//  Recipes.swift
+//  2210990385_test2
+//
+//  Created by student-2 on 23/11/24.
+//
+
+import Foundation
